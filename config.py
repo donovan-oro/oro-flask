866306f02a3ed1911a2be12803ff276f20c0cd83
Novandev <donovan.adams@students.makeschool.com>
2019-08-26 12:18:28 -0700

    added more code to push

---
+++ config.py
@@ -0,0 +1,9 @@
+from commercetools import Client
+
+client = Client(
+    project_key=“oro-marketplace-89“,
+    client_id=“q81HDnASx-yili8brpRD6VV7“,
+    client_secret=“AItez-9cWOeCg-LsZGX47EY4vHXpnnw2“,
+    scope=[“< scopes >“],
+    url=“https: // api.commercetools.co“,
+    token_url=“https: // auth.commercetools.co/oauth/token”,)
