952c3d496129ad31ed1a3c58d47fca8a811322ca
donovan-oro <donovan-oro>
2019-08-27 03:45:32 -0700

    Added more toall infor in format that canbe used

--- app.py
+++ app.py
@@ -27,16 +27,72 @@ def list_products(auth, project_key):
     url = "https://api.commercetools.co/%s/products" % project_key
     r = requests.get(url, headers=headers)
     products = r.json()
-    product_dict = {
-        'name': '',
-        'description': '',
-        'sku': '',
+    product_list = []
+    for i in range(len(products["results"])):
+        product_dict = {
+            'name': '',
+            'description': '',
+            'sku': '',
+            'prices': [],
+            'images': [],
+            'seller': '',
+            'is-oro-certified': '',
+            'benefits': ''}
+        print(products["results"][i]['masterData']['current'])
+        product_dict['name'] = products["results"][i]['masterData']['current']['name']['en']
+        product_dict['description'] = products["results"][i]['masterData']['current']['description']['en']
+        product_dict['sku'] = products["results"][i]['masterData']['current']['masterVariant']['sku']
+        [product_dict['prices'].append(price['value']['centAmount'])
+         for price in products["results"][i]['masterData']['current']['masterVariant']['prices']]
 
-    }
-    print(products["results"][0]['masterData']['current'])
-    product_dict['name'] =
+        [product_dict['images'].append(image['url'])
+         for image in products["results"][i]['masterData']['current']['masterVariant']['images']]
+        print('\n')
+        for val in products["results"][i]['masterData']['current']['masterVariant']['attributes']:
+            if val['name'] == 'seller':
+                product_dict['seller'] = val['value']
+
+            if val['name'] == 'is-oro-certified':
+                product_dict['is-oro-certified'] = val['value']
+
+            if val['name'] == 'cbd':
+                product_dict['cbd'] = val['value']
+
+            if val['name'] == 'aroused':
+                product_dict['aroused'] = val['value']
+
+            if val['name'] == 'rested':
+                product_dict['rested'] = val['value']
+
+            if val['name'] == 'calm':
+                product_dict['calm'] = val['value']
+
+            if val['name'] == 'happy':
+                product_dict['happy'] = val['value']
+
+            if val['name'] == 'pain-relief':
+                product_dict['pain-relief'] = val['value']
+
+            if val['name'] == 'beautiful':
+                product_dict['beautiful'] = val['value']
+
+            if val['name'] == 'energized':
+                product_dict['energized'] = val['value']
+
+            if val['name'] == 'focused':
+                product_dict['focused'] = val['value']
+
+            if val['name'] == 'general-wellness':
+                product_dict['general-wellness'] = val['value']
+
+            if val['name'] == 'food-beverages':
+                product_dict['food-beverages'] = val['value']
+        # print('\nFor this product, the schema looks like\n')
+        product_list.append(product_dict)
+    print(product_list)
 
 
+# ['name', 'description', 'sku', 'prices', 'images', 'seller', 'is-oro-certified', 'benefits', 'cbd', 'aroused', 'rested', 'calm', 'happy', 'pain-relief', 'beautiful', 'energized', 'focused', 'general-wellness', 'food-beverages']
 auth = login(config.CLIENT_ID, config.CLIENT_SECRET, config.PROJECT_KEY)
 
 list_products(auth, config.PROJECT_KEY)
